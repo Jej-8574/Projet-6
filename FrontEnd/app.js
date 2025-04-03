c2d260e9f99eca83a919fca4cebe793f1cb6ca47
Jérémy Catoire <j.catoire60@gmail.com>
2025-04-03 19:07:08 +0200

    Début filtres

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -7,8 +7,7 @@ async function getWorks() {
       }
       
       const json = await response.json();
-      console.log(json, "coucoutest");
-  
+      
       for (let i = 0; i < json.length; i++) {
         setFigure(json[i]);
       }
@@ -16,19 +15,42 @@ async function getWorks() {
     } catch (error) {
       console.error(error.message);
     }
-  }
-  
+}
+  getWorks();
+
   function setFigure(data) {
-    const gallery = document.querySelector(".gallery");
+  const figure = document.createElement("figure");
+    figure.innerHTML = `<img src="${data.imageUrl}" alt="${data.title}">
+                      <figcaption>${data.title}</figcaption>`;
   
-    const figure = document.createElement("figure");
-    figure.innerHTML = `
-      <img src="${data.imageUrl}" alt="${data.title}">
-      <figcaption>${data.title}</figcaption>
-    `;
-  
-    gallery.append(figure);
+    document.querySelector(".gallery").append(figure);
   }
     
-  getWorks();
+  async function getCategories() {
+    const url = "http://localhost:5678/api/categories";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      
+      const json = await response.json();
+      console.log(json);
+      
+      for (let i = 0; i < json.length; i++) {
+        setFilter(json[i]);
+      }
   
+    } catch (error) {
+      console.error(error.message);
+    }
+}
+
+getCategories()
+
+function setFilter(data) {
+  const div = document.createElement("figure");
+    div.innerHTML = `${data.name}`;
+  
+    document.querySelector(".div-container").append(div);
+}
