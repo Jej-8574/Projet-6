5f0cd597c3f13fab290440c78e86891e35dd104b
Jérémy Catoire <j.catoire60@gmail.com>
2025-04-17 19:38:23 +0200

    ajout login.js

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,3 +1,21 @@
-const loginApi = "https://localhost:5678/user/login";
+const loginApi = "http://localhost:5678/api/users/login";
 
-function handleSubmit(email, password) {}
+async function handleSubmit() {
+  let user = {
+    email: "sophie.bluel@test.tld",
+    password: "S0phie",
+  };
+
+  let response = await fetch(loginApi, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  });
+
+  let result = await response.json();
+  console.log(result);
+}
+
+handleSubmit();
