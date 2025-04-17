4660a59f836bbeeaa1b0992bf1bf5b58d0e82ef5
Jérémy Catoire <j.catoire60@gmail.com>
2025-04-17 20:09:47 +0200

    login js 50%

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,9 +1,13 @@
 const loginApi = "http://localhost:5678/api/users/login";
 
-async function handleSubmit() {
+document.getElementById("loginform").addEventListener("submit", handleSubmit);
+
+async function handleSubmit(event) {
+  event.preventDefault();
+
   let user = {
-    email: "sophie.bluel@test.tld",
-    password: "S0phie",
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value
   };
 
   let response = await fetch(loginApi, {
@@ -17,5 +21,3 @@ async function handleSubmit() {
   let result = await response.json();
   console.log(result);
 }
-
-handleSubmit();
